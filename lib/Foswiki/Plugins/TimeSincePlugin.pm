460db1cf05ee6d4492d5f221085ae80d1419b737
MichaelDaum <github@michaeldaumconsulting.com>
2014-11-17 14:06:05 +0100

    Item13097: remove dependency on CPAN modules
    This fixes duration maths and removes some of DateTime's limitations.

--- lib/Foswiki/Plugins/TimeSincePlugin.pm
+++ lib/Foswiki/Plugins/TimeSincePlugin.pm
@@ -1,14 +1,7 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2005-2009 Michael Daum http://michaeldaumconsulting.com
+# Copyright (C) 2005-2014 Michael Daum http://michaeldaumconsulting.com
 # 
-# Adapted from WordPress plugin TimeSince by
-# Michael Heilemann (http://binarybonsai.com), 
-# Dunstan Orchard (http://www.1976design.com/blog/archive/2004/07/23/redesign-time-presentation/),
-# Nataile Downe (http://blog.natbat.co.uk/archive/2003/Jun/14/time_since)
-# 
-# Thanks to all of you!!!
-#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
@@ -22,22 +15,16 @@
 package Foswiki::Plugins::TimeSincePlugin;
 
 use strict;
-use vars qw( 
-  $VERSION $RELEASE $NO_PREFS_IN_TOPIC $SHORTSUMMARY
-  $isInitialized
-);
+use warnings;
 
-$VERSION = '$Rev$';
-$RELEASE = '3.00';
-$NO_PREFS_IN_TOPIC = 1;
-$SHORTSUMMARY = 'Display time difference in a human readable way';
+our $VERSION = '4.00';
+our $RELEASE = '4.00';
+our $NO_PREFS_IN_TOPIC = 1;
+our $SHORTSUMMARY = 'Display time difference in a human readable way';
+our $isInitialized;
 
 ###############################################################################
 sub initPlugin {
-
-  if ($Foswiki::Plugins::VERSION < 1.1) {
-    return 0;
-  }
 
   Foswiki::Func::registerTagHandler('TIMESINCE', \&handleTimeSince);
   return 1;
--- lib/Foswiki/Plugins/TimeSincePlugin/Core.pm
+++ lib/Foswiki/Plugins/TimeSincePlugin/Core.pm
@@ -1,13 +1,6 @@
 # Plugin for Foswiki - The Free and Open Source Wiki, http://foswiki.org/
 #
-# Copyright (C) 2005-2009 Michael Daum http://michaeldaumconsulting.com
-# 
-# Adapted from WordPress plugin TimeSince by
-# Michael Heilemann (http://binarybonsai.com), 
-# Dunstan Orchard (http://www.1976design.com/blog/archive/2004/07/23/redesign-time-presentation/),
-# Nataile Downe (http://blog.natbat.co.uk/archive/2003/Jun/14/time_since)
-# 
-# Thanks to all of you!!!
+# Copyright (C) 2005-2014 Michael Daum http://michaeldaumconsulting.com
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -21,347 +14,142 @@
 
 package Foswiki::Plugins::TimeSincePlugin::Core;
 
-use DateTime;
 use strict;
+use warnings;
 
-use constant DEBUG => 0; # toggle me
+use Foswiki::Func ();
+use Foswiki::Time();
+use POSIX;
 
-our %MON2NUM = (
-  Jan => 1,
-  Feb => 2,
-  Mar => 3,
-  Apr => 4,
-  May => 5,
-  Jun => 6,
-  Jul => 7,
-  Aug => 8,
-  Sep => 9,
-  Oct => 10,
-  Nov => 11,
-  Dec => 12
+use constant TRACE => 0;    # toggle me
+
+our %SECONDSOFUNIT = (
+  years   => 31536000,
+  months  => 2628000,
+  weeks   => 604800,
+  days    => 86400,
+  hours   => 3600,
+  minutes => 60,
+  seconds => 1,
 );
 
 ###############################################################################
 sub writeDebug {
-  print STDERR "TimeSincePlugin - $_[0]\n" if DEBUG
+  print STDERR "TimeSincePlugin - $_[0]\n" if TRACE;
 }
 
 ###############################################################################
 sub handleTimeSince {
-  my ($session, $params, $theTopic, $theWeb) = @_;
+  my ($session, $params, $topic, $web) = @_;
 
   #writeDebug("handleTimeSince(" . $params->stringify() . ") called");
 
+  my $theTopic = $params->{topic} || $topic;
+  my $theWeb = $params->{web} || $web;
   my $theFrom = $params->{_DEFAULT} || $params->{from} || '';
   my $theTo = $params->{to} || '';
-  my $theUnits = $params->{units} || 2;
+  my $theUnits = $params->{units};
+  $theUnits = 2 unless defined $theUnits;
 
-  my $theSeconds = $params->{seconds} || 'off';
-  my $theMinutes = $params->{minutes} || 'on';
-  my $theHours = $params->{hours} || 'on';
-  my $theDays = $params->{days} || 'on';
-  my $theWeeks = $params->{weeks} || 'on';
-  my $theMonths = $params->{months} || 'on';
-  my $theYears = $params->{years} || 'on';
+  my %doUnit = ();
+  my $numUnits;
 
-  my $theAbs = $params->{abs} || 'off';
+  if ($theUnits =~ /^(\d+)$/) {
+    $numUnits = $1;
+  
+    $doUnit{seconds} = Foswiki::Func::isTrue($params->{seconds}, 0);
+    $doUnit{minutes} = Foswiki::Func::isTrue($params->{minutes}, 1);
+    $doUnit{hours} = Foswiki::Func::isTrue($params->{hours}, 1);
+    $doUnit{days} = Foswiki::Func::isTrue($params->{days}, 1);
+    $doUnit{weeks} = Foswiki::Func::isTrue($params->{weeks}, 1);
+    $doUnit{months} = Foswiki::Func::isTrue($params->{months}, 1);
+    $doUnit{years} = Foswiki::Func::isTrue($params->{years}, 1);
+  } else {
+    %doUnit = map {$_ => 1} split(/\s*,\s*/, $theUnits);
+  }
+    
+
+  my $theAbs = Foswiki::Func::isTrue($params->{abs}, 0);
   my $theNull = $params->{null} || 'about now';
   my $theFormat = $params->{format} || '$time';
   my $theNegFormat = $params->{negformat} || '- $time';
 
-  if ($theFrom eq '' && $theTo eq '') {
-    # if there's no starting date then get the current revision date
-    my ($date) = Foswiki::Func::getRevisionInfo($theWeb, $theTopic);
-    $theFrom = DateTime->from_epoch(epoch=>$date);
-    $theTo = parseTime();
+  if ($theFrom eq '') {
+    ($theWeb, $theTopic) = Foswiki::Func::normalizeWebTopicName($theWeb, $theTopic);
+    ($theFrom) = Foswiki::Func::getRevisionInfo($theWeb, $theTopic);
+  } else {
+    $theFrom =~ s/^\s+|\s+$//go;
+
+    unless ($theFrom =~ /^\d+$/) {
+      my $epoch = Foswiki::Time::parseTime($theFrom);
+      return inlineError("ERROR: can't parse from=\"$theFrom\"") unless defined $epoch;
+      $theFrom = $epoch;
+    }
+  }
+
+  if ($theTo eq '') {
+    $theTo = time();
   } else {
 
-    $theFrom =~ s/^\s*(.*)\s*$/$1/go;
-    $theTo =~ s/^\s*(.*)\s*$/$1/go;
-
-  
-    # convert time to DateTime object
-    if ($theFrom ne '') {
-      if ($theFrom =~ /^\d+$/) { # already epoch seconds
-        $theFrom = DateTime->from_epoch(epoch=>$theFrom);
-      } else {
-	eval {
-	  local $SIG{'__DIE__'};
-	  $theFrom = parseTime($theFrom);
-	};
-	if ($@) {
-	  my $message = $@;
-	  $message =~ s/\scallback\s.*$//gs;
-	  return &inlineError("ERROR: can't parse from=\"$theFrom\" - $message");
-	}
-	return &inlineError("ERROR: can't parse from=\"$theFrom\"")
-	  unless defined $theFrom;
-      }
-    } else {
-      $theFrom = parseTime();
-    }
-    if ($theTo ne '') {
-      if ($theTo =~ /^\d+$/) { # already epoch seconds
-        $theTo = DateTime->from_epoch(epoch=>$theTo);
-      } else {
-	eval {
-	  local $SIG{'__DIE__'};
-	  $theTo = parseTime($theTo);
-	};
-	if ($@) {
-	  my $message = $@;
-	  $message =~ s/\scallback\s.*$//gs;
-	  return &inlineError("ERROR: can't parse to=\"$theTo\" - $message");
-	}
-	return &inlineError("ERROR: can't parse from=\"$theTo\"")
-	  unless defined $theTo;
-      }
-    } else {
-      $theTo = parseTime();
+    $theTo =~ s/^\s+|\s+$//go;
+    if ($theTo !~ /^\d+$/) {    # already epoch seconds
+      my $epoch = Foswiki::Time::parseTime($theTo);
+      return inlineError("ERROR: can't parse from=\"$theTo\"") unless defined $epoch;
+      $theTo = $epoch;
     }
   }
 
-  #writeDebug("theFrom=$theFrom, theTo=$theTo");
+  writeDebug("theFrom=$theFrom, theTo=$theTo");
 
-  my $since = $theTo - $theFrom;
-  my $isNeg = $since->is_negative;
-  $since = $since->inverse if $isNeg;
+  my $duration = $theTo - $theFrom;
+  my $isNeg = $duration < 0;
+  $duration = abs($duration) if $isNeg;
 
-  # calculate time string
-  my $timeString = '';
-  my $unit;
-
-  my @units = ();
-  my $last;
-  foreach my $unit (qw(years months weeks days hours minutes seconds)) {
-    next if $theSeconds eq 'off' && $unit eq 'seconds';
-    next if $theMinutes eq 'off' && $unit eq 'minutes';
-    next if $theHours eq 'off' && $unit eq 'hours';
-    next if $theDays eq 'off' && $unit eq 'days';
-    next if $theWeeks eq 'off' && $unit eq 'weeks';
-    next if $theMonths eq 'off' && $unit eq 'months';
-    next if $theYears eq 'off' && $unit eq 'years';
-    push @units, $unit;
-    $last = $unit;
-  }
-
-  my @raw = $since->in_units(@units);
-
+  # gather result string
+  my @result = ();
   my $index = 0;
-  foreach my $unit (@units) {
+  foreach my $unit (qw(years months weeks days hours minutes seconds)) {
+    next unless $doUnit{$unit};
 
-    my $count = shift @raw;
+    my $factor = $SECONDSOFUNIT{$unit};
+    my $count = floor($duration / $factor);
+    writeDebug("duration=$duration, unit=$unit, seconds in $unit=$factor, count=$count");
+
     if ($count) {
-      if ($index > 0) {
-        $timeString .= ($unit eq $last)?' and ':', ';
-      }
-      $unit =~ s/s$//go if $count == 1;
-      $timeString .= "$count $unit";
+      my $label = $unit;
+      $label =~ s/s$//go if $count == 1; # TODO use maketext
+      push @result, "$count $label";
+
       $index++;
-      last if $index >= $theUnits;
-    } 
-    #writeDebug("unit=$unit, count=$count, timestring=$timeString");
-  };
+      $duration -= $count * $factor;
 
-  #writeDebug("timeString=$timeString");
-
-  if ($timeString eq '') {
-    return expandVariables($theNull);
-  } else {
-    my $format = $isNeg?$theNegFormat:$theFormat;
-    return expandVariables($format, 'time'=>$timeString);
+      last if defined $numUnits && $index >= $numUnits;
+    }
   }
-}
 
-###############################################################################
-sub parseTime {
-    my $date = shift;
+  my $timeString = '';
+  if (@result) {
+    my $last = pop @result;
 
+    $timeString = join(", ", @result) . ' and ' if @result;
+    $timeString .= $last;
+  }
 
-    # about now
-    return DateTime->from_epoch(
-      epoch=>time(), 
-      time_zone=>'local') unless $date;
+  my $result;
+  if ($timeString eq '') {
+    $result = $theNull;
+  } else {
+    $result = $isNeg ? $theNegFormat : $theFormat;
+    $result =~ s/\$time/$timeString/g;
+  }
 
-    #writeDebug("parseTime($date)");
-
-    my $tz = ($date =~ /GMT$/i)?'GMT':'floating';
-
-    #writeDebug("tz=$tz");
-
-    # NOTE: This routine *will break* if input is not one of below formats!
-    
-    # FIXME - why aren't ifs around pattern match rather than $5 etc
-    # try "31 Dec 2001 - 23:59:11"  (Foswiki date)
-    if ($date =~ /(\d+)\s+([A-Za-z]+)\s+(\d\d\d\d)[\s\-]+(\d+)\:(\d+)(?:\:(\d+))?/) {
-        my $year = $3;
-        my $seconds = $6 || 0;
-        #$year -= 1900 if( $year > 1900 );
-        # The ($2) will look up the constant so named
-        #writeDebug("case 1");
-        return DateTime->new(
-          second=> $seconds, 
-          minute=> $5, 
-          hour=>$4, 
-          day=>$1, 
-          month=>$MON2NUM{$2}, 
-          year=>$year,
-          time_zone=>$tz
-        ) 
-    }
-
-    # try "31 Dec 2001"
-    if ($date =~ /(\d+)\s+([A-Za-z]+)\s+(\d\d\d\d)/) {
-        my $year = $3;
-        #$year -= 1900 if( $year > 1900 );
-        # The ($2) will look up the constant so named
-        #writeDebug("case 2");
-        return DateTime->new( 
-          second=>0, 
-          minute=>0,
-          hour=>0, 
-          day=>$1, 
-          month=>$MON2NUM{$2}, 
-          year=>$year,
-          time_zone=>$tz
-        );
-    }
-
-    # try "2001/12/31 23:59:59" or "2001.12.31.23.59.59" (RCS date)
-    if ($date =~ /(\d\d\d\d)[\.\/\-](\d+)[\.\/\-](\d+)[\.\s\-]+(\d+)[\.\:](\d+)[\.\:](\d+)/) {
-        my $year = $1;
-        #$year -= 1900 if( $year > 1900 );
-        #writeDebug("case 3");
-        return DateTime->new( 
-          second=>$6, 
-          minute=>$5, 
-          hour=>$4, 
-          day=>$3, 
-          month=>$2, 
-          year=>$year,
-          time_zone=>$tz
-        );
-    }
-
-    # try "2001/12/31 23:59" or "2001.12.31.23.59" (RCS short date)
-    if ($date =~ /(\d\d\d\d)[\.\/\-](\d+)[\.\/\-](\d+)[\.\s\-]+(\d+)[\.\:](\d+)/) {
-        my $year = $1;
-        #$year -= 1900 if( $year > 1900 );
-        #writeDebug("case 4");
-        return DateTime->new( 
-          second=>0, 
-          minute=>$5, 
-          hour=>$4, 
-          day=>$3, 
-          month=>$2, 
-          year=>$year,
-          time_zone=>$tz
-        );
-    }
-
-    # try "2001/12/31"
-    if ($date =~ /(\d\d\d\d)[\.\/\-](\d+)[\.\/\-](\d+)/) {
-        my $year = $1;
-        #$year -= 1900 if( $year > 1900 );
-        #writeDebug("case 5");
-        return DateTime->new( 
-          second=>0, 
-          minute=>0, 
-          hour=>0, 
-          day=>$3, 
-          month=>$2, 
-          year=>$year,
-          time_zone=>$tz
-        );
-    }
-
-    # try "12/31/2001"
-    if ($date =~ /(\d+)[\.\/\-](\d+)[\.\/\-](\d\d\d\d)/) {
-        my $month = $1;
-        my $day = $2;
-        my $year = $3;
-        if ($day > 12) {
-          my $tmp = $month;
-          $month = $day;
-          $day = $tmp;
-        }
-        #$year -= 1900 if( $year > 1900 );
-        #writeDebug("case 6");
-        return DateTime->new( 
-          second=>0, 
-          minute=>0, 
-          hour=>0, 
-          day=>$3, 
-          month=>$2, 
-          year=>$year,
-          time_zone=>$tz
-        );
-    }
-
-
-    # ISO date
-    if ($date =~ /(\d\d\d\d)(?:-(\d\d)(?:-(\d\d))?)?(?:T(\d\d)(?::(\d\d)(?::(\d\d(?:\.\d+)?))?)?)?(Z|[-+]\d\d(?::\d\d)?)?/ ) {
-      my ($Y, $M, $D, $h, $m, $s, $myTz) =
-        ($1, $2||1, $3||1, $4||0, $5||0, $6||0, $7||'');
-      $M--;
-      $Y -= 1900 if( $Y > 1900 );
-      $tz = $myTz if defined $myTz;
-      #writeDebug("case 7");
-      return DateTime->new(
-        second=>$s, 
-        minute=>$m, 
-        hour=>$h, 
-        day=>$D, 
-        month=>$M, 
-        year=>$Y,
-        time_zone=>$tz,
-      );
-    }
-
-    # try "2001-12-31T23:59Z" or "2001-12-31T23:59+01:00" (ISO short date)
-    # FIXME: Calc local to zulu time "2001-12-31T23:59+01:00"
-    if ($date =~ /(\d+)\-(\d+)\-(\d+)T(\d+)\:(\d+)/ ) {
-        my $year = $1;
-        #$year -= 1900 if( $year > 1900 );
-        #writeDebug("case 8");
-        return DateTime->new( 
-          second=>0, 
-          minute=>$5, 
-          hour=>$4, 
-          day=>$3, 
-          month=>$2, 
-          year=>$year,
-          time_zone=>$tz,
-        );
-    }
-
-    # give up, return start of epoch (01 Jan 1970 GMT)
-    return undef;
+  return Foswiki::Func::decodeFormatTokens($result);
 }
 
 ###############################################################################
 sub inlineError {
-  return '<span class="foswikiAlert">' . $_[0] . '</span>' ;
+  return '<div class="foswikiAlert">' . $_[0] . '</div>';
 }
 
-###############################################################################
-sub expandVariables {
-  my ($theFormat, %params) = @_;
-
-  return '' unless $theFormat;
-  
-  foreach my $key (keys %params) {
-    $theFormat =~ s/\$$key/$params{$key}/g;
-  }
-  $theFormat =~ s/\$percnt/\%/go;
-  $theFormat =~ s/\$t\b/\t/go;
-  $theFormat =~ s/\$nop//g;
-  $theFormat =~ s/\$n/\n/go;
-  $theFormat =~ s/\$dollar/\$/go;
-
-  return $theFormat;
-}
-
-###############################################################################
 1;
 
